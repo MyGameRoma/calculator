581abd93d9f18b5785269ed35aaab0e2116b2f9d
roman <roman.mihailov9@gmail.com>
2019-09-24 20:24:46 +0300

    simple calkulator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,27 +8,43 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] args) {
-        double a = Double.parseDouble( args[0]);
+        String a = args[0];
         String op = args[1];
-        double b = Double.parseDouble(args[2]);
+        String b = args [2];
+        String op2 = args [3];
+        String c = args [4];
+
+
+        var result1 = calc(args[0], args[1], args[2]);
+        var result2 = calc(Double.toString(result1), op2, args[4]);
+
+        return Double.toString(result2);
+    }
+
+
+    double calc(String stra, String op, String strb) {
+        double a = Double.parseDouble(stra);
+        double b = Double.parseDouble(strb);
         double result;
         switch (op) {
-            case "+": result = a + b;
+            case "+":
+                result = a + b;
                 break;
-            case "-": result = a - b;
+            case "-":
+                result = a - b;
                 break;
-            case "*": result = a * b;
+            case "*":
+                result = a * b;
                 break;
-            case "/": result = a / b;
+            case "/":
+                result = a / b;
                 break;
 
             default:
                 result = 0;
 
+
         }
-
-
-        return "" + result;
+        return result;
     }
-
 }
